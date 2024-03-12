6887c570e27b4a7a4835319b5e887ae82a020f0c
Andrew Wagner <andrew@skylight.digital>
2024-03-11 21:16:06 -0400

    Add smaller adjacency list

--- loaddata.rb
+++ loaddata.rb
@@ -3,8 +3,11 @@ require 'CSV'
 
 Node.destroy_all
 
-File.open('./nodes.csv') do |file|
+File.open('./nodes2.csv') do |file|
   nodes = CSV.parse(file)
   nodes.shift
-  nodes.each {|node| Node.create!(id: node[0], node_id: node[1])}
+  nodes.each do |node|
+    node_id = node[1].present? ? node[1].to_i : nil
+    Node.create!(id: node[0].to_i, node_id:)
+  end
 end
