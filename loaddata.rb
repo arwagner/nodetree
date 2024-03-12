4657684acfe6eac59d7034676f148c8972fb1f2f
Andrew Wagner <andrew@skylight.digital>
2024-03-12 04:33:11 -0400

    Improve loaddata automation

--- loaddata.rb
+++ loaddata.rb
@@ -1,13 +1,19 @@
 require './server.rb'
 require 'CSV'
 
-Node.destroy_all
+def load_data(path_to_file)
+  Node.destroy_all
 
-File.open('./nodes2.csv') do |file|
-  nodes = CSV.parse(file)
-  nodes.shift
-  nodes.each do |node|
-    node_id = node[1].present? ? node[1].to_i : nil
-    Node.create!(id: node[0].to_i, node_id:)
+  File.open(path_to_file) do |file|
+    nodes = CSV.parse(file)
+    nodes.shift
+    nodes.each do |node|
+      node_id = node[1].present? ? node[1].to_i : nil
+      Node.create!(id: node[0].to_i, node_id:)
+    end
   end
+end
+
+if __FILE__ == $0
+  load_data('./nodes2.csv')
 end
--- nodes_spec.rb
+++ nodes_spec.rb
@@ -1,96 +1,101 @@
 require './server.rb'
 require 'rspec'
+require './loaddata'
 
-describe "#depth" do
-  subject { depth(Node.find(node_id)) }
+describe "server" do
+  before(:all) { load_data('./nodes2.csv') }
 
-  context "for root nodes" do
-    let(:node_id) { 130 }
+  describe "#depth" do
+    subject { depth(Node.find(node_id)) }
 
-    it "returns 1" do
-      expect(subject).to eq(1)
+    context "for root nodes" do
+      let(:node_id) { 130 }
+
+      it "returns 1" do
+        expect(subject).to eq(1)
+      end
+    end
+
+    context "for sub-sub-nodes" do
+      let(:node_id) { 4430546 }
+
+      it "returns 3 for sub-sub-nodes" do
+        expect(subject).to eq(3)
+      end
     end
   end
 
-  context "for sub-sub-nodes" do
-    let(:node_id) { 4430546 }
+  describe "#lowest_common_ancestor" do
+    subject { lowest_common_ancestor(Node.find(a), Node.find(b)) }
 
-    it "returns 3 for sub-sub-nodes" do
-      expect(subject).to eq(3)
+    context "descendant of sibling" do
+      let(:a) { 5497637 }
+      let(:b) { 2820230 }
+
+      it "returns the parent" do
+        expect(subject.id).to eq(125)
+      end
     end
-  end
-end
 
-describe "#lowest_common_ancestor" do
-  subject { lowest_common_ancestor(Node.find(a), Node.find(b)) }
+    context "descendant" do
+      let(:a) { 5497637 }
+      let(:b) { 130 }
 
-  context "descendant of sibling" do
-    let(:a) { 5497637 }
-    let(:b) { 2820230 }
-
-    it "returns the parent" do
-      expect(subject.id).to eq(125)
+      it "returns the ancestor" do
+        expect(subject.id).to eq(130)
+      end
     end
-  end
 
-  context "descendant" do
-    let(:a) { 5497637 }
-    let(:b) { 130 }
+    context "child" do
+      let(:a) { 5497637 }
+      let(:b) { 4430546 }
 
-    it "returns the ancestor" do
-      expect(subject.id).to eq(130)
+      it "returns the parent" do
+        expect(subject.id).to eq(4430546)
+      end
     end
-  end
 
-  context "child" do
-    let(:a) { 5497637 }
-    let(:b) { 4430546 }
+    context "self" do
+      let(:a) { 4430546 }
+      let(:b) { 4430546 }
 
-    it "returns the parent" do
-      expect(subject.id).to eq(4430546)
+      it "returns itself" do
+        expect(subject.id).to eq(4430546)
+      end
     end
-  end
 
-  context "self" do
-    let(:a) { 4430546 }
-    let(:b) { 4430546 }
-
-    it "returns itself" do
-      expect(subject.id).to eq(4430546)
-    end
-  end
-
-  context "node that doesn't exist" do
-    it "returns nil" do
-      expect(lowest_common_ancestor(nil, Node.find(4430546))).to be_nil
-    end
-  end
-end
-
-describe "#root" do
-  subject { root(Node.find(node_id)) }
-
-  context "a root node" do
-    let(:node_id) { 130 }
-
-    it "returns the same node" do
-      expect(subject.id).to eq(node_id)
+    context "node that doesn't exist" do
+      it "returns nil" do
+        expect(lowest_common_ancestor(nil, Node.find(4430546))).to be_nil
+      end
     end
   end
 
-  context "a child" do
-    let(:node_id) { 125 }
+  describe "#root" do
+    subject { root(Node.find(node_id)) }
 
-    it "returns the root" do
-      expect(subject.id).to eq(130)
+    context "a root node" do
+      let(:node_id) { 130 }
+
+      it "returns the same node" do
+        expect(subject.id).to eq(node_id)
+      end
     end
-  end
 
-  context "a child" do
-    let(:node_id) { 5497637 }
+    context "a child" do
+      let(:node_id) { 125 }
 
-    it "returns the root" do
-      expect(subject.id).to eq(130)
+      it "returns the root" do
+        expect(subject.id).to eq(130)
+      end
+    end
+
+    context "a child" do
+      let(:node_id) { 5497637 }
+
+      it "returns the root" do
+        expect(subject.id).to eq(130)
+      end
     end
   end
 end
